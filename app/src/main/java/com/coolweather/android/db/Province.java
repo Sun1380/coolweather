2c03b0fede140c32ed1d355802693ff824afea70
Sun1380 <sun5201380@gmai.com>
2018-09-05 17:24:04 +0800

    完成遍历市省三级列表的功能.

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -10,23 +10,18 @@ public class Province extends DataSupport {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getProvinceName() {
         return provinceName;
     }
-
     public void setProvinceName(String provinceName) {
         this.provinceName = provinceName;
     }
-
     public int getProvinceCode() {
         return provinceCode;
     }
-
     public void setProvinceCode(int provinceCode) {
         this.provinceCode = provinceCode;
     }
